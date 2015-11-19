622d249af87ab201152882be24ab2990a57be075
Scott Frederick <scottyfred@gmail.com>
2015-11-19 13:05:46 -0600

    Update to Spring Boot 1.3.0.

--- src/main/java/org/cloudfoundry/sample/shutdown/Application.java
+++ src/main/java/org/cloudfoundry/sample/shutdown/Application.java
@@ -2,14 +2,13 @@ package org.cloudfoundry.sample.shutdown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
